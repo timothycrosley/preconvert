33cd0eaf33954453082bb5eac8e995227c474759
Timothy Crosley <timothy.crosley@gmail.com>
2019-07-17 17:52:30 -0700

    Bump version

--- preconvert/__init__.py
+++ preconvert/__init__.py
@@ -1,5 +1,5 @@
 """A Library to enable preconversion of any Python type into one that is easily serializable"""
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
 from preconvert import convert, exceptions, output
 from preconvert.convert import unserializable
