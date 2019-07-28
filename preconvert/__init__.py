1b39cda78e53a23cf271087ab72aa1b257a68a1c
Timothy Crosley <timothy.crosley@gmail.com>
2019-07-27 23:51:21 -0700

    Add initial automatic plugin loading support

--- preconvert/__init__.py
+++ preconvert/__init__.py
@@ -1,10 +1,14 @@
 """A Library to enable preconversion of any Python type into one that is easily serializable"""
-__version__ = "0.0.2"
+import pkg_resources
 
 from preconvert import convert, exceptions, output
 from preconvert.convert import default_serializer
 from preconvert.register import bson, converter, json, msgpack
 
+for plugin in pkg_resources.iter_entry_points("preconvert.converters"):
+    plugin.load()
+
+__version__ = "0.0.2"
 __all__ = [
     "converter",
     "json",
@@ -14,4 +18,5 @@ __all__ = [
     "convert",
     "output",
     "unserializable",
+    "__version__",
 ]
