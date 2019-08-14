d01a5603da35f31141866efa6632e7f991553e8e
Timothy Crosley <timothy.crosley@gmail.com>
2019-08-13 23:54:13 -0700

    Fixes to automatic documentation

--- update_docs.sh
+++ update_docs.sh
@@ -2,7 +2,7 @@
 mkdir -p docs/__auto__
 pdoc3 preconvert > docs/__auto__/preconvert.md
 pdoc3 preconvert.convert > docs/__auto__/preconvert.convert.md
-pdoc3 preconvert.converters > docs/__auto__/preconvert.preconvert.converters.md
+pdoc3 preconvert.converters > docs/__auto__/preconvert.converters.md
 pdoc3 preconvert.exceptions > docs/__auto__/preconvert.exceptions.md
 pdoc3 preconvert.register > docs/__auto__/preconvert.register.md
 pdoc3 preconvert.output > docs/__auto__/preconvert.output.md
