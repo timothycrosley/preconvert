f1e9da98cbde3e8048d0c3db47beea0cd5a3354d
Timothy Crosley <timothy.crosley@gmail.com>
2019-08-15 18:58:56 -0700

    Update how docs are generated

--- update_docs.sh
+++ update_docs.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-mkdir -p docs/__auto__
-pdoc3 preconvert > docs/__auto__/preconvert.md
-pdoc3 preconvert.convert > docs/__auto__/preconvert.convert.md
-pdoc3 preconvert.converters > docs/__auto__/preconvert.converters.md
-pdoc3 preconvert.exceptions > docs/__auto__/preconvert.exceptions.md
-pdoc3 preconvert.register > docs/__auto__/preconvert.register.md
-pdoc3 preconvert.output > docs/__auto__/preconvert.output.md
+pdoc3 -o docs preconvert --template-dir templates --force -c show_type_annotations=True
 mkdocs gh-deploy
