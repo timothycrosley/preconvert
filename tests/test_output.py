6ccbf980e19d492a04a294781028e92a823c6084
Timothy Crosley <timothy.crosley@gmail.com>
2019-08-18 06:13:34 -0700

    Trigger print out of variable

--- tests/test_output.py
+++ tests/test_output.py
@@ -60,6 +60,7 @@ def test_json():
     crazy_object_json = preconvert.output.json.loads(preconvert.output.json.dumps(MyCrazyObject()))
     assert crazy_object_json == "Like anyone could convert this"
 
+    assert preconvert.output.json.dumps({"data": ["Τη γλώσσα μου έδωσαν ελληνική"]}) == "test"
     print(preconvert.output.json.dumps({"data": ["Τη γλώσσα μου έδωσαν ελληνική"]}, ensure_ascii=False))
     assert preconvert.output.json.loads(
         preconvert.output.json.dumps({"data": ["Τη γλώσσα μου έδωσαν ελληνική"]}, ensure_ascii=False)
