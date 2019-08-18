cd5140f0972b772c25b659d550a4b6c9b1315753
Timothy Crosley <timothy.crosley@gmail.com>
2019-08-18 11:55:10 -0700

    Print offending line for more information

--- tests/test_output.py
+++ tests/test_output.py
@@ -48,6 +48,7 @@ def test_json():
         "1234.25",
     ]
 
+    print(assert preconvert.output.json.loads(preconvert.output.json.dumps(b"a", ensure_ascii=True)))
     # assert preconvert.output.json.loads(preconvert.output.json.dumps(b"a", ensure_ascii=True)) == "a"
 
     class MyCrazyObject(object):
