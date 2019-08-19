8dc4d85c7c266a0f149135ead4737df6b64ec6a2
Timothy Crosley <timothy.crosley@gmail.com>
2019-08-18 17:35:03 -0700

    Attempt to add documentation auto regeneration

--- update_docs.sh
+++ update_docs.sh
@@ -2,5 +2,5 @@
 
 wget https://raw.githubusercontent.com/hugapi/HOPE/master/all/HOPE-8--Style-Guide-for-Hug-Code.md -O docs/CODING_STANDARD.md
 wget https://raw.githubusercontent.com/hugapi/HOPE/master/all/HOPE-11-Code-of-Conduct.md -O docs/CODE_OF_CONDUCT.md
-pdoc3 -o docs preconvert --template-dir templates --force -c show_type_annotations=True
-mkdocs gh-deploy
+pipenv run pdoc3 -o docs preconvert --template-dir templates --force -c show_type_annotations=True
+pipenv run mkdocs gh-deploy
